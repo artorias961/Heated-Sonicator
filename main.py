a4134a1d91b634018cbfdad6b971555cebb72e86
artorias961 <artorias961@gmail.com>
2022-11-08 13:24:18 -0800

    code works

--- main.py
+++ main.py
@@ -1,78 +1,100 @@
-from machine import Pin, I2C
-import ssd1306
+import board
 import time
+import displayio
+import terminalio
+import adafruit_ahtx0
+from adafruit_display_text import label
+import adafruit_displayio_sh1107
 
-def user_interface_prompt():
-    # Prompting the user to input the time for how long the machine should run
-    print("Input the hr/mm for the machine to run")
+def temp_and_humidity():
+    # Use for I2c for Temp/Humidity sensor
+    i2c = board.I2C()
+    sensor = adafruit_ahtx0.AHTx0(i2c)
     
-    # Button one will toggle between hr/mm (NOT DEFINE PIN)
-    toggle_button = Pin(13, Pin.IN)
+    # Getting the temperature from the sensor
+    temperature = sensor.temperature
     
-    # Button 2 will increment the time (NOT DEFINE PIN)
-    increment_time_button = Pin(14, Pin.IN)
+    # Getting the humidity from the sensor 
+    humidity = sensor.relative_humidity
     
-    # Button 3 will decrement the time (NOT DEFINE PIN)
-    decrement_time_button = Pin(15, Pin.IN)
+    # Convert datatype from float to int
+    temperature = int(temperature)
+    humidity = int(humidity)
     
-    # Button 4 will start the machine(NOT DEFINE PIN)
-    set_button = Pin(16, Pin.IN)
+    return temperature, humidity
     
-    # Creating a while loop for the user to input there time
-    while True:
-        # Creating a temp button for a frequncy response
-        temp = 0
-        hour = 0
-        minutes = 0
-        
-        # This will be regarding for hour
-        # if temp == 0 && toggle_button.value() == 0:
-        
-        # Creating an if statement to increase the hour
-        if increment_time_button.value() > 0 and decrement_time_button ==0:
-            hour = hour + 1
-        # Creating an if statement to drease the hour
-        if decrement_time_button.value() > 0 and increment_time_button == 0:
-            hour = hour - 1
-        # 
-        if hour < 0:
-            print("The value has to be great than 0 !!!")
-            print("We have increase the hour by 1")
-            hour = hour + 1
-        if hour >= 9:
-            print("You have exceeded the maximum hours, lower your hours")
-            print("We have decrease the hour by 1")
-            hour = hour - 1
 
+def display(temp: int, humid: int):
+    
+    # Calling the OLED screen
+    displayio.release_displays()
 
-def oled_screen():
-    # Input pin for I2C
-    sda = Pin(4)
-    
-    # Output pin for I2C
-    scl = Pin(5)
-    
-    # Checking the speed rate for I2C (Default Address)
-    i2c = I2C(sda,scl)
-    
-    # Oled library i2c
-    oled_display = ssd1306.SSD1306_I2C(128, 64, i2c)
-    
-    # Testing out a print statement
-    oled_display.text("Hello World!!!", 0, 0, 1)
-    oled_display.show()
-    
-    
-    # For testing purposes
-    led = Pin(13, Pin.OUT)
-    while True:
-        led.on()
-        time.sleep(0.5)
-        led.off()
-        time.sleep(0.5)
+    # Use for I2C for LCD
+    i2c = board.I2C()
+    display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
 
+    # SH1107 is vertically oriented 64x128
+    WIDTH = 128
+    HEIGHT = 64
+    BORDER = 2
 
+    # Setting up the configuration for the display
+    display = adafruit_displayio_sh1107.SH1107(
+        display_bus,     # I2C bus
+        width=WIDTH,     # Max Width
+        height=HEIGHT,   # Max height
+        rotation=0)      # No rotation
 
-def main():
-    user_interface_prompt()
-    oled_screen()
+    # Make the display context
+    splash = displayio.Group()
+    display.show(splash)
+
+    # Setting up the color bits for the OLED Display (Color format)
+    color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
+    color_palette = displayio.Palette(1)
+    color_palette[0] = 0xFFFFFF  # White Pixels
+
+    # Creating the outline box on the OLED Display
+    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+    splash.append(bg_sprite)
+
+    # Draw a smaller inner rectangle in black
+    inner_bitmap = displayio.Bitmap(WIDTH - BORDER * 2, HEIGHT - BORDER * 2, 1)
+    inner_palette = displayio.Palette(1)
+    inner_palette[0] = 0x000000  # Black
+    inner_sprite = displayio.TileGrid(
+        inner_bitmap,
+        pixel_shader=inner_palette,
+        x=BORDER,
+        y=BORDER)
+    splash.append(inner_sprite)
+
+    # Draw some label text
+    text1 = "Arty Chan T-T"
+    text_area = label.Label(terminalio.FONT,  # font style
+                            text=text1,       # text string
+                            color=0xFFFFFF,   # white pixels
+                            x=8,              # x-axis position
+                            y=8               # y-axis position
+                            )                 
+    
+    splash.append(text_area)
+    text2 = f"Temperature: {temp}C\nHumidity: {humid}%"
+    text_area2 = label.Label(
+        terminalio.FONT, # font style
+        text=text2,      # text string
+        scale=1,         # font size
+        color=0xFFFFFF,  # white pixel
+        x=9,             # x-axis position
+        y=20             # y-axis position
+    )
+    splash.append(text_area2)
+
+while True:
+    temp, humid = temp_and_humidity()
+    
+    print(f"Temp: {temp} C, Humid: {humid} %")
+    time.sleep(2)
+    display(temp, humid)
+    time.sleep(2)
+    
