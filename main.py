ba1a2361ed6d46e75ffc4733fac12cd285aeee59
artorias961 <artorias961@gmail.com>
2022-11-09 08:18:40 -0800

    Adding OUTPUT Pin for LED

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import board
 import time
 import displayio
+from digitalio import DigitalInOut, Direction, Pull 
 import terminalio
 import adafruit_ahtx0
 from adafruit_display_text import label
@@ -73,6 +74,7 @@ def display(temp: int, humid: int):
     text1 = "   Arty Chan T-T"
     text_area = label.Label(terminalio.FONT,  # font style
                             text=text1,       # text string
+                            scale=1,          # font size
                             color=0xFFFFFF,   # white pixels
                             x=8,              # x-axis position
                             y=8               # y-axis position
@@ -89,12 +91,56 @@ def display(temp: int, humid: int):
         y=20             # y-axis position
     )
     splash.append(text_area2)
+    
+def led_setup(temp):
+    # Pin setup for overload water level
+    water_level_overload_led = DigitalInOut(33)
+    water_level_overload_led.direction = digitalio.Direction.OUTPUT
+    
+    # Pin setup for underload water level
+    water_level_underload_led = DigitalInOut(15)
+    water_level_underload_led.direction = digitalio.Direction.OUTPUT
+    
+    # Pin setup for max temperature limit
+    max_temp_led = DigitalInOut(32)
+    max_temp_led.direction = digitalio.Direction.OUTPUT
+    
+    return water_level_overload_led, water_level_underload_led, max_temp_led
 
-while True:
-    temp, humid = temp_and_humidity()
+def water_level_status_led(temp, humid, water_level_overload_led, water_level_underload_led, max_temp_led):
+    # Checking for the temperature limit 
+    if 70 > temp:
+        max_temp_led.value = True
+        stop_switch = True
+        
+    return stop_switch
+        
+        
+def main():
+    # Setting up the pins
+    water_level_overload_led, water_level_underload_led, max_temp_led = led_setup()
     
-    print(f"Temp: {temp} C, Humid: {humid} %")
-    time.sleep(2)
-    display(temp, humid)
-    time.sleep(2)
+    # Infinite Statement
+    while True:
+        # Getting the updated temperature and humidity sensor data
+        temp, humid = temp_and_humidity()
+        
+        # Testing to see if the code works and prints on the computer console 
+        print(f"Temp: {temp} C, Humid: {humid} %")
+        
+        # Waits for 2 mili-seconds for the data
+        time.sleep(2)
+        
+        # Send the data to the display oled module
+        display(temp, humid)
+        
+        # Send the pins and the sensor data to check 
+        stop_switch = water_level_status_led(temp, humid, water_level_overload_led, water_level_underload_led, temp_overload_led)
+        
+        # Waits for 2 mili-seconds for the data 
+        time.sleep(2)
+        
+        if !(stop_switch):
+            break
     
+
